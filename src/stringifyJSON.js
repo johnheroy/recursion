f4f606414066be03b716cfc9a10f95b015d38284
John Heroy <johnheroy@gmail.com>
2014-08-18 13:47:03 +0800

    Pass first few tests

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,25 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === null) {
+    return 'null';
+  } else if (typeof obj === 'string') {
+    return '\"' + obj + '\"';
+  } else if (Array.isArray(obj)) {
+    // is an array
+  } else if (typeof obj === 'object') {
+    // is an object
+  } else {
+    return new String(obj).toString();
+  }
+
+  return _.reduce(obj, function(memo, val, key, list) {
+    if (Array.isArray(val)) {
+      // is an array
+    } else if (typeof val === 'object') {
+      // is an object
+    } else {
+      return memo += val;
+    }
+  }, '');
 };
